3d6ff6545e4d8b217e3762d3bd8837c22191bd94
oliullahlimon <oliullah.limon.55@gmail.com>
2024-06-06 16:53:57 +0600

    Assignment 4 is completed

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,14 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import User from './User';
 
 const Users = (props) => {
+  const { users } = props;
   return (
     <section className="users">
-      {props.users.map((user) => (
+      {users.map((user) => (
         <User key={user.id} {...user} />
       ))}
     </section>
